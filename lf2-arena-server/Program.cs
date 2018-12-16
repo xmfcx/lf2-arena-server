be194a8e0286121e9776b7f100bef53f14f5a728
M. Fatih CIRIT <mfcirit@gmail.com>
2018-12-16 16:05:13 +0300

    gitignore, project

---
+++ lf2-arena-server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace lf2_arena_server
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("wow");
+    }
+  }
+}
